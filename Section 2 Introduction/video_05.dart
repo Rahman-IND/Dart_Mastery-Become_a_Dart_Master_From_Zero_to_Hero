b5560f81f8d235f0eff4386cea5c2eeec32e9c36
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-12 17:40:26 +0530

    Add video_05.dart

---
+++ Section 2 Introduction/video_05.dart
@@ -0,0 +1,9 @@
+/*Topic: Explaination about main method and print statement */
+
+void main() {
+  print("Hello World");
+  print("This is our first Dart programming application");
+  print(6);
+  print(6 + 3);
+  print(true);
+}
