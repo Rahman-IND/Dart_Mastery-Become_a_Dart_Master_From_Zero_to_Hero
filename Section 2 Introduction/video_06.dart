a51f3157add1f0d4dd23015222293732c56d66d8
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-12 17:52:57 +0530

    add video_06.dart

---
+++ Section 2 Introduction/video_06.dart
@@ -0,0 +1,21 @@
+/*Topic: Different types of Comments: Single line(//), multi-line(/**/), Documentation comment(///)*/
+
+void main() {
+  /*This is our multi line comment and can 
+  be used for better understanding*/
+
+  //This is our hello world application.
+  print("Hello World");
+
+  //This is our first application.
+  print("This is our first dart programming application");
+
+  //Integer value
+
+  ///This is our demo application
+  ///You can use integer, String, boolean values in print statement.
+
+  print(6);
+  print(6 + 3);
+  print(false);
+}
