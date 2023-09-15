223b83b2cbffd21f792acb99fecddc80c62efb61
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-15 20:40:47 +0530

    Add video_20.dart

---
+++ Section 5 Iterative Statements/video_20.dart
@@ -0,0 +1,9 @@
+/*Topic : For In Loop Statement */
+
+void main() {
+  // For In Loop
+  var fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+  for ( var fruit in fruits){
+    print(fruit);
+  }
+}
