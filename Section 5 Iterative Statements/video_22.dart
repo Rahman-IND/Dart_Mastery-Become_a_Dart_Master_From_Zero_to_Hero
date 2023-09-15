789f460f6948d8243db1403420adcb56aec14907
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-15 21:09:25 +0530

    add video_22.dart

---
+++ Section 5 Iterative Statements/video_22.dart
@@ -0,0 +1,12 @@
+/* Topic : Do while Loop Statement.
+It's like post increment operator, 
+the value will print first then increment */
+
+void main() {
+  // Do while Loop
+  var num = 1;
+  do {
+    print(num);
+    num = num + 1;
+  } while (num <= 10);
+}
