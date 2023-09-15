2eb8e23588ffaada6a9c47cb32744bd17528ae98
Motinur Rahman <motinurrahman.dev@gmail.com>
2023-09-15 20:52:48 +0530

    add video_21.dart

---
+++ Section 5 Iterative Statements/video_21.dart
@@ -0,0 +1,11 @@
+/*Topic : While Loop Statement */
+
+void main() {
+  var table = 8;
+  var num = 1;
+  var to = 10;
+  while (num <= to) {
+    print("${table} x ${num} = ${table * num}");
+    num++;
+  }
+}
